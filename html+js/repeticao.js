96cce36395782ad36655da2aa00b1b4a0c5a6a36
MaxSousa <max3456ty@gmail.com>
2026-04-28 14:39:38 -0300

    while

---
+++ html+js/repeticao.js
@@ -0,0 +1,9 @@
+let cont = 0;
+while (cont <= 10){
+    console.log(cont)
+    cont++
+}
+do {
+    console.log(cont)
+    cont++
+} while (cont<=10);
--- html+js/switchCase.js
+++ html+js/switchCase.js
@@ -6,16 +6,16 @@ let option = parseInt(prompt("escolha uma operação (1.+ 2.- 3.* 4./)"))
 
 switch (option) {
     case 1:
-        alert("o resultado é ", n1 + n2)
+        alert("o resultado é "+ (n1 + n2))
         break
     case 2:
-        alert("o resultado é ", n1 - n2)
+        alert("o resultado é "+ (n1 - n2))
         break
     case 3:
-        alert("o resultado é ", n1 * n2)
+        alert("o resultado é "+ (n1 * n2))
         break
     case 4:
-        alert("o resultado é ", n1/n2)
+        alert("o resultado é "+ (n1/n2))
         break
     default:
         alert("erro")
